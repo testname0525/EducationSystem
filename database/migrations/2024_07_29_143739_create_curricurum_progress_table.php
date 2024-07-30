586a2ad87bfb8e084c286dfae8dbb22677ee0569
ayumi matsushita <aym.tksg.bbc1024@gmail.com>
2024-07-30 22:52:21 +0900

    マイグレーションファイルを追加: users と curriculum_progress テーブル

--- database/migrations/2024_07_29_143739_create_curricurum_progress_table.php
+++ database/migrations/2024_07_29_143739_create_curricurum_progress_table.php
@@ -6,26 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('curricurum_progress', function (Blueprint $table) {
+        Schema::create('curriculum_progress', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('curriculum_id');
+            $table->unsignedBigInteger('user_id');
+            $table->tinyInteger('clear_flg');
             $table->timestamps();
+
+            $table->foreign('curriculum_id')->references('id')->on('curriculums');
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('curricurum_progress');
+        Schema::dropIfExists('curriculum_progress');
     }
 };
