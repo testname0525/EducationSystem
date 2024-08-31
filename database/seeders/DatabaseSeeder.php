7f9303ed943ff30cf4082f6e47dcf0ff3d5a0b3e
大久保　桂悟 <k.okubo19881125@gmail.com>
2024-09-01 07:26:24 +0900

    ファクトリーにてデータベースにバナー画像投入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        \App\Models\Banner::factory(3) -> create();
     }
 }
